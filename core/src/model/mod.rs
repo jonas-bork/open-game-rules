b10ef3b8aa9ece548906d028cbda913fa09d5cf0
Jonas Bork <jonas10bork@gmail.com>
2026-06-13 22:36:41 +0200

    removed all example code

--- core/src/model/mod.rs
+++ core/src/model/mod.rs
@@ -9,23 +9,16 @@ use crate::{
 #[derive(Default)]
 pub struct Model {
     pub game_rules: GameRules,
-    pub count: isize,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum Event {
     Start,
-    Increment,
-    Decrement,
-    Reset,
 }
 
 impl Model {
     pub fn update(&mut self, event: Event) -> crux_core::Command<Effect, Event> {
         match event {
-            Event::Increment => self.count += 1,
-            Event::Decrement => self.count -= 1,
-            Event::Reset => self.count = 0,
             Event::Start => {
                 self.game_rules =
                     game_rules::load_all_rules().expect("failed to deserialize the generated games")
