2f7effad8d82e2cb81825fc34716c0e766cace48
Jonas Bork <jonas10bork@gmail.com>
2026-08-06 17:08:32 +0200

    added equipment and tags badges

--- shells/web/src/components/games_overview.rs
+++ shells/web/src/components/games_overview.rs
@@ -1,8 +1,15 @@
 use leptos::prelude::*;
 use open_game_rules_core::{Equipment, view::GamesOverviewViewModel};
+use phosphor_leptos::{Icon, IconData};
 
 use crate::{
-    components::{common::button::Button, use_dispatch},
+    components::{
+        common::{
+            badge::{Badge, BadgeVariant},
+            button::Button,
+        },
+        use_dispatch,
+    },
     core::Event,
 };
 
@@ -22,14 +29,25 @@ pub fn games_overview_view(#[prop(into)] vm: Signal<GamesOverviewViewModel>) -> 
                         <div class="mb-4 rounded-xl border border-gray-200 bg-white shadow-sm transition-shadow hover:shadow-md">
                             <div class="p-5 flex flex-col items-start">
                                 <p class="mb-4 text-lg font-semibold text-gray-800">{rule.name}</p>
-                                <div class="flex flex-row">
-                                    // TODO: Add equipment, players and also difficulty (also add the last one to the core)
-                                    <div class="rounded-md bg-rose-500">
-                                        <span>{rule.equipment.first().unwrap().clone()}</span>
+
+                                // Badges
+                                <div class="flex flex-col gap-x-2 gap-y-1">
+                                    <div class="flex flex-row gap-x-2 gap-y-1">
+                                        // TODO: Add equipment, players and also difficulty (also add the last one to the core)
+                                        <Badge variant=BadgeVariant::Blue>
+                                            <Icon icon=phosphor_leptos::PACKAGE size="18px" />
+                                            <span>{rule.equipment.into_iter().next().unwrap()}</span>
+                                        </Badge>
                                     </div>
-                                </div>
-                                <div class="flex flex-row">
-                                    // TODO: add tags
+                                    <div class="flex flex-row gap-x-2 gap-y-1">
+                                        {
+                                            rule.tags.into_iter().map(|tag| {
+                                                view! {
+                                                    <Badge>{tag.clone()}</Badge>
+                                                }
+                                            }).collect::<Vec<_>>()
+                                        }
+                                    </div>
                                 </div>
 
                                 <Button
