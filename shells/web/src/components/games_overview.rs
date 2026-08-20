73711fe6f601b9d8f9a152830105c4a4590b3b7e
Jonas Bork <59069012+jonas-bork@users.noreply.github.com>
2026-08-20 07:47:59 +0200

    feat(ui): replace back buttons with carets for more intuitive controls (#80)

--- shells/web/src/components/games_overview.rs
+++ shells/web/src/components/games_overview.rs
@@ -1,5 +1,6 @@
 use leptos::prelude::*;
 use open_game_rules_core::view::GamesOverviewViewModel;
+use phosphor_leptos::Icon;
 
 use crate::{
     components::{common::button::Button, game_badges::GameBadges, use_dispatch},
@@ -19,18 +20,18 @@ pub fn games_overview_view(#[prop(into)] vm: Signal<GamesOverviewViewModel>) -> 
             {move || {
                 vm.read().clone().game_rules.into_iter().map(|(id, rule)| {
                     view! {
-                        <div class="mb-4 rounded-xl border border-gray-200 bg-white shadow-sm transition-shadow hover:shadow-md">
-                            <div class="p-5 flex flex-col items-start gap-2">
-                                <p class="text-lg font-semibold text-gray-800">{rule.name}</p>
+                        <div class="mb-4 rounded-xl border border-gray-200 bg-white shadow-sm transition-shadow hover:shadow-md"
+                            on:click=move |_| {
+                                dispatch.run(Event::GameOverview(GamesOverviewEvent::SelectGame(id.clone())));
+                            }
+                        >
+                            <div class="p-5 flex flex-col items-start gap-2 cursor-pointer">
+                                <div class="flex flex-row items-center text-lg font-semibold text-gray-800">
+                                    <span>{rule.name}</span>
+                                    <Icon icon=phosphor_leptos::CARET_RIGHT size="20px" />
+                                </div>
 
                                 <GameBadges equipment={rule.equipment} players={rule.players} tags={rule.tags} complexity={rule.complexity} playing_time={rule.playing_time} />
-
-                                <Button
-                                    label="Details"
-                                    on_click=UnsyncCallback::new(move |()| {
-                                        dispatch.run(Event::GameOverview(GamesOverviewEvent::SelectGame(id.clone())));
-                                    })
-                                />
                             </div>
                         </div>
                     }
