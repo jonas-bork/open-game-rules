9da3961fbec17d72679ad5ed2b95106513ed6162
Jonas Bork <jonas10bork@gmail.com>
2026-08-22 11:43:04 +0200

    feat: implement IconButton for back button on game details page

---
+++ shells/web/src/components/common/icon_button.rs
@@ -0,0 +1,44 @@
+use leptos::prelude::*;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum Color {
+    // Filled,
+    // Tonal
+    //Outlined,
+    Standard,
+}
+
+impl Color {
+    const fn classes(self) -> &'static str {
+        match self {
+            Self::Standard => {
+                "text-on-surface-variant hover:bg-on-surface-variant/[8%] focus-visible:bg-on-surface-variant/[10%] focus-visible:outline focus-visible:outline-[3px] focus-visible:outline-offset-2 focus-visible:outline-secondary active:bg-on-surface-variant/10"
+            }
+        }
+    }
+}
+
+#[component]
+pub fn icon_button(
+    color: Color,
+    #[prop(into)] on_click: Callback<()>,
+    #[prop(optional, into)] style: Option<String>,
+    children: Children,
+) -> impl IntoView {
+    let class = format!(
+        "flex items-center justify-center p-1 rounded-full transition-color duration-200 cursor-pointer {}",
+        color.classes()
+    );
+
+    view! {
+        <button
+            class=class
+            style=style
+            on:click=move |_| {
+                on_click.run(());
+            }
+        >
+            {children()}
+        </button>
+    }
+}
